c0da871b8ad67560076ec7f940230dd48ac3a492
jianjamesastrero@gmail.com <jiana@goetu.com>
2020-04-08 01:39:34 +0800

    tentative main ui

--- app/src/main/java/com/jianastrero/appetiser_apps_coding_challenge/activities/MainActivity.kt
+++ app/src/main/java/com/jianastrero/appetiser_apps_coding_challenge/activities/MainActivity.kt
@@ -2,7 +2,10 @@ package com.jianastrero.appetiser_apps_coding_challenge.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
 import com.jianastrero.appetiser_apps_coding_challenge.R
+import com.jianastrero.appetiser_apps_coding_challenge.extensions.dp
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
