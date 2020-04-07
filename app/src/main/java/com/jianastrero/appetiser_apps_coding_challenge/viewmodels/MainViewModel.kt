6801d4aeb681ca637ef5f7d0d5bbc1473898b64c
jianjamesastrero@gmail.com <jiana@goetu.com>
2020-04-08 05:10:28 +0800

    update data to have genre filtered, trending and featured

--- app/src/main/java/com/jianastrero/appetiser_apps_coding_challenge/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/jianastrero/appetiser_apps_coding_challenge/viewmodels/MainViewModel.kt
@@ -4,7 +4,8 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.jianastrero.appetiser_apps_coding_challenge.SETTINGS_LAST_VISIT
 import com.jianastrero.appetiser_apps_coding_challenge.binding.NonNullObservableField
-import com.jianastrero.appetiser_apps_coding_challenge.models.Result
+import com.jianastrero.appetiser_apps_coding_challenge.extensions.random
+import com.jianastrero.appetiser_apps_coding_challenge.models.Movie
 import com.jianastrero.appetiser_apps_coding_challenge.repositories.iTunesSearchRepository
 import com.jianastrero.appetiser_apps_coding_challenge.singletons.Settings
 import kotlinx.coroutines.CoroutineScope
@@ -19,19 +20,44 @@ class MainViewModel(
     val lastVisit = NonNullObservableField("")
     val search = NonNullObservableField("")
 
-    val list = mutableListOf<Result>()
+    val filteredGenreList = mutableListOf<Pair<String, List<Movie>>>()
+    val trendingList = mutableListOf<Movie>()
+    var featured: Movie? = null
 
     fun reset() {
         lastVisit.set(Settings.get(SETTINGS_LAST_VISIT, "Never"))
-        list.clear()
+        filteredGenreList.clear()
+        trendingList.clear()
+        featured = null
     }
 
     fun fetchData() = CoroutineScope(Dispatchers.IO).launch {
         val search = iTunesSearchRepository.search()
 
-        val results = search.results.toMutableList().sortedBy { it.trackName }
+        val results =
+            search.movies
+                .sortedBy { it.trackName }
+                .toMutableList()
 
-        list.clear()
-        list.addAll(results)
+        val mapped =
+            results
+                .distinctBy { it.primaryGenreName }
+                .map {
+                    it.primaryGenreName to
+                        results
+                            .filter { item ->
+                                it.primaryGenreName == item.primaryGenreName
+                            }
+                            .toMutableList()
+                            .sortedBy { item -> item.trackName }
+                }
+
+        trendingList.clear()
+        trendingList.addAll(results.random(10))
+
+        filteredGenreList.clear()
+        filteredGenreList.addAll(mapped)
+
+        featured = results.random()
     }
 }
