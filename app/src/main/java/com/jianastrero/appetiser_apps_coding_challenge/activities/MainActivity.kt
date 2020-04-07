40305fefd33d8ae457eb6c86e8713544150f3e82
jianjamesastrero@gmail.com <jiana@goetu.com>
2020-04-07 22:17:21 +0800

    move mainactivity to activities package

--- app/src/main/java/com/jianastrero/appetiser_apps_coding_challenge/activities/MainActivity.kt
+++ app/src/main/java/com/jianastrero/appetiser_apps_coding_challenge/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jianastrero.appetiser_apps_coding_challenge
+package com.jianastrero.appetiser_apps_coding_challenge.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jianastrero.appetiser_apps_coding_challenge.R
 
 class MainActivity : AppCompatActivity() {
 
