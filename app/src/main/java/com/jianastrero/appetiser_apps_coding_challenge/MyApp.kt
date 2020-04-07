1ceb16f670e7594ed1249738d8482ff5a6d13e14
jianjamesastrero@gmail.com <jiana@goetu.com>
2020-04-07 22:18:02 +0800

    create and set the class for the application

---
+++ app/src/main/java/com/jianastrero/appetiser_apps_coding_challenge/MyApp.kt
@@ -0,0 +1,12 @@
+package com.jianastrero.appetiser_apps_coding_challenge
+
+import android.app.Application
+
+class MyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // TODO
+    }
+}
